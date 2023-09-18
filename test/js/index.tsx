42e11a724a0535004ee32db291a61515478474d3
David Lougheed <david.lougheed@gmail.com>
2023-09-18 10:46:23 -0400

    test: add test site with webpack builder

---
+++ test/js/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import 'leaflet/dist/leaflet.css';
+import '../../src/styles.css';
+
+import BentoChoroplethMap from '../../src/Components/Maps/BentoChoroplethMap';
+import { TEST_HEATMAP_GEOJSON_FEATURES } from './test_data';
+
+const BentoChartsTestApp = () => {
+  return (
+    <div>
+      <h1>Bento Charts Test App</h1>
+      <BentoChoroplethMap
+        features={TEST_HEATMAP_GEOJSON_FEATURES}
+        categoryProp="pop"
+        data={[{x: "AB", y: 50}, {x: "NB", y: 75}, {x: "SB", y: 60}]}
+        colorMode={{
+          mode: "continuous",
+          minColor: "rgba(122, 122, 255, 0.2)",
+          maxColor: "rgba(255, 122, 122, 0.5)",
+        }}
+        onClick={(f) => {
+          console.log(f);
+          alert(JSON.stringify(f, null, 2));
+        }}
+        height={600}
+        center={[74.0694163, -112.7217838]}
+        zoom={2.75}
+      />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <BentoChartsTestApp />
+);
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  mode: 'development',
+  entry: './test/js/index.tsx',
+  output: {
+    path: __dirname + '/test/dist',
+    publicPath: '',
+    filename: 'js/[name][chunkhash].js',
+  },
+  module: {
+    rules: [
+      { test: /\.[tj](sx|s)?$/, use: { loader: 'ts-loader' }, exclude: /node_modules/ },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  watchOptions: {
+    poll: 1000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      inject: false,
+      template: 'test/index.ejs',
+    }),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: './test/dist',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
+
+module.exports = (_env, argv) => {
+  if (argv.mode === 'development') {
+    config.devtool = 'inline-source-map';
+  }
+  return config;
+};
