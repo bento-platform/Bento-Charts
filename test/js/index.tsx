b0452a03d0ce06900ca346d2b648d37110bd001e
David Lougheed <david.lougheed@gmail.com>
2023-09-19 10:10:17 -0400

    test(maps): use antd for styling + add point map test

--- test/js/index.tsx
+++ test/js/index.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { Card, Layout, Tabs, TabsProps, Typography } from 'antd';
+
+import 'antd/dist/reset.css';
 import 'leaflet/dist/leaflet.css';
 import '../../src/styles.css';
 
 import BentoChoroplethMap from '../../src/Components/Maps/BentoChoroplethMap';
 import { TEST_HEATMAP_GEOJSON_FEATURES } from './testData';
+import BentoPointMap from '../../src/Components/Maps/BentoPointMap';
 
-const BentoChartsTestApp = () => {
-  return (
-    <div>
-      <h1>Bento Charts Test App</h1>
+const items: TabsProps['items'] = [
+  {
+    key: 'choropleth',
+    label: 'Map: Choropleth',
+    children: (
       <BentoChoroplethMap
         features={TEST_HEATMAP_GEOJSON_FEATURES}
         categoryProp="pop"
@@ -29,7 +34,38 @@ const BentoChartsTestApp = () => {
         zoom={2.75}
         renderPopupBody={(_f, d) => <>{d} samples</>}
       />
-    </div>
+    ),
+  },
+  {
+    key: 'points',
+    label: 'Map: Points',
+    children: (
+      <BentoPointMap
+        data={TEST_HEATMAP_GEOJSON_FEATURES.features[1].geometry.coordinates[0].map((c, i) => {
+          console.log(c);
+          return {
+            title: `point ${i}`,
+            coordinates: c as [number, number],
+          };
+        })}
+        height={600}
+        center={[74.0694163, -112.7217838]}
+        zoom={2.75}
+      />
+    ),
+  }
+]
+
+const BentoChartsTestApp = () => {
+  return (
+    <Layout>
+      <Layout.Content style={{ padding: 24, height: "100vh" }}>
+        <Card>
+          <Typography.Title level={1}>Bento Charts Test App</Typography.Title>
+          <Tabs items={items} />
+        </Card>
+      </Layout.Content>
+    </Layout>
   );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ const config = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+      },
     ],
   },
   watchOptions: {
