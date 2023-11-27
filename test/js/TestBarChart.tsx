c95afffcad9a211fb23a44edd5eca31d319f69ee
David Lougheed <david.lougheed@gmail.com>
2023-11-27 11:37:19 -0500

    test: add responsive bar chart to test app

--- test/js/TestBarChart.tsx
+++ test/js/TestBarChart.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
+import { Typography } from 'antd';
 import { BarChart } from '../../src/index';
 
 const TestBarChart = () => (
-  <BarChart
-    data={[{x: "AB", y: 50}, {x: "NB", y: 75}, {x: "SB", y: 60}]}
-    units="management units"
-    onClick={(f) => {
-      console.log(f);
-      alert(JSON.stringify(f, null, 2));
-    }}
-    height={600}
-    width={960}
-  />
+  <>
+    <Typography.Title level={2}>Responsive bar chart:</Typography.Title>
+    <BarChart
+      data={[{x: "AB", y: 50}, {x: "NB", y: 75}, {x: "SB", y: 60}, {x: "AU", y: 30}, {x: "XA", y: 80}]}
+      units="management units"
+      height={600}
+    />
+    <Typography.Title level={2}>Fixed-width bar chart:</Typography.Title>
+    <BarChart
+      data={[{x: "AB", y: 50}, {x: "NB", y: 75}, {x: "SB", y: 60}]}
+      units="management units"
+      onClick={(f) => {
+        console.log(f);
+        alert(JSON.stringify(f, null, 2));
+      }}
+      height={600}
+      width={960}
+    />
+  </>
 );
 
 export default TestBarChart;
