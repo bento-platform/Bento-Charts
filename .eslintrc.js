c606927bf57fbd9f8dbee1868bf87820e191a0e9
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2024-03-06 14:25:29 -0500

    chore(dev): Ran Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,23 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    semi: [2, 'always'],
+  },
+  settings: {
+    react: {
+      version: '^18.2.0',
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint",
-        "prettier",
-    ],
-    "rules": {
-        "prettier/prettier": "error",
-        "semi": [2, "always"]
-    },
-    "settings": {
-        "react": {
-            "version": "^18.2.0"
-        }
-    }
-}
+  },
+};
