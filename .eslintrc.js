a5d4bce66e5f9353d21c9220b39ff16a37367fa4
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2024-05-30 14:28:43 -0400

    refactor(chartTypes): Resolved lint eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,29 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended",
-        "plugin:react-hooks/recommended",
-        "plugin:prettier/recommended"
-    ],
-    "plugins": [
-        "@typescript-eslint",
-        "react",
-        "react-hooks",
-        "prettier"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
-    },
-    "rules": {
-        "prettier/prettier": "error",
-        "react/prop-types": "off",
-        "semi": [2, "always"]
-    }
-}
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'react/prop-types': 'off',
+    semi: [2, 'always'],
+  },
+};
