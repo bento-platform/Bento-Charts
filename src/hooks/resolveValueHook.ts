75e3576c5e6eabf2e924452b1f593a63bc26b865
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2024-05-30 12:28:01 -0400

    refactor(resolveValueHook): Resolved lint issues

--- src/hooks/resolveValueHook.ts
+++ src/hooks/resolveValueHook.ts
@@ -1,4 +1,4 @@
-const useResolvedValue = <T,>(value: T | undefined, defaultValueHook: () => T): T => {
+const useResolvedValue = <T>(value: T | undefined, defaultValueHook: () => T): T => {
   const defaultValue = defaultValueHook();
   return value !== undefined ? value : defaultValue;
 };
